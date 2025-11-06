25f887e4757f889ec83da6c067e5fc9244ae581e
meguro0525se-bit <meguro.0525se@gmail.com>
2025-11-06 20:57:48 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       $user_score1=80;
+       $user_score2=60;
+       $user_score3=55;
+       $user_score4=40;
+       $user_score5=100;
+       $user_score6=25;
+       $user_score7=80;
+       $user_score8=95;
+       $user_score9=30;
+       $user_score10=60;
+       
+       $user_score1 + $user_score2 + $user_score3 + $user_score4 + $user_score5 + $user_score6 + $user_score7 + $user_score8 + $user_score9 + $user_score10;
+       
+       echo ($user_score1 + $user_score2 + $user_score3 + $user_score4 + $user_score5 + $user_score6 + $user_score7 + $user_score8 + $user_score9 + $user_score10)/10;  
+       ?>
+   </p>
+</body>
+
+</html>
